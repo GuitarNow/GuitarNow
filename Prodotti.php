89ee8cd70525bcca3a73c0242dbdb3c30939e084
Damiano <damicodamiano10@gmail.com>
2020-12-23 00:03:14 +0100

    NavBar sistemata eliminando il menù a tendina

--- Prodotti.php
+++ Prodotti.php
@@ -1,19 +1,15 @@
 <?php
 
+$categoria = $_REQUEST['categoria'];
+
 require_once("PHP/back/ManageProdotti.php");
 
 $web_page = file_get_contents('html/template.html');
 
 $web_page = str_replace('<title_page/>', "Prodotti", $web_page);
 
-$nav_bar = '       <li  class="link" role="none" xml:lang="en"><a href="../Home.html" role="menuitem">Home</a></li> 
-<li role="none" class="link">
-    <a id="tendina" role="menuitem" aria-haspopup="true" aria-expanded="false">Prodotti</a>
-    <ul role="menu" class="tendinaContenuto">
-        <li id="linkCorrente" role="none">Chitarre</li>
-        <li role="none"><a href="Accessori.html" role="menuitem" tabindex="-1">Accessori</a></li>
-    </ul>
-</li>
+$nav_bar = '       <li  class="link" role="none" xml:lang="en"><a href="Home.html" role="menuitem">Home</a></li> 
+<li  id="linkCorrente" class="link" role="none">Prodotti</li>
 <li class="link" role="none"><a href="Servizi.html" role="menuitem">Servizi</a></li>
 <li class="link" role="none"><a href="Chisiamo.html" role="menuitem">Chi siamo</a></li>';
 $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
@@ -21,10 +17,16 @@ $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
 $web_page = str_replace('<breadcrumbs_to_insert/>', "Prodotti/Chitarre", $web_page);
 
-$chitare_da_visualizzare = '<ul class="chitCard">';
+$chitare_da_visualizzare 
+= '<ul class="chitCard">';
 
 $chitarre_manage = new ManageProdotti();
-$chitarre_database = $chitarre_manage->get_chitarra();
+if($categoria == "accessori"){
+    $chitarre_database = $chitarre_manage->get_accessori();
+}else{
+    $chitarre_database = $chitarre_manage->get_chitarra();
+}
+
 
 foreach($chitarre_database as $chitarre)
 {
