bc9d8b87e7dfafdd7fb1f595d4eed4e333c54575
matteobudai <tisco99@hotmail.it>
2020-12-23 15:26:41 +0100

    spostamento file #21

--- Prodotti.php
+++ Prodotti.php
@@ -8,10 +8,10 @@ $web_page = file_get_contents('html/template.html');
 
 $web_page = str_replace('<title_page/>', "Prodotti", $web_page);
 
-$nav_bar = '       <li  class="link" role="none" xml:lang="en"><a href="Home.html" role="menuitem">Home</a></li> 
+$nav_bar = '       <li  class="link" role="none" xml:lang="en"><a href="Home.php" role="menuitem">Home</a></li> 
 <li  id="linkCorrente" class="link" role="none">Prodotti</li>
-<li class="link" role="none"><a href="Servizi.html" role="menuitem">Servizi</a></li>
-<li class="link" role="none"><a href="Chisiamo.html" role="menuitem">Chi siamo</a></li>';
+<li class="link" role="none"><a href="Servizi.php" role="menuitem">Servizi</a></li>
+<li class="link" role="none"><a href="Chisiamo.php" role="menuitem">Chi siamo</a></li>';
 $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
 
