3101100189c1a8a25469003fb166b5c2448ffce8
Racca <raccanello.matteo@gmail.com>
2020-12-24 12:22:03 +0100

    ulteriori modifiche a visualizza_prodotto.php

--- Visualizza_prodotto.php
+++ Visualizza_prodotto.php
@@ -4,7 +4,12 @@ require_once('PHP/back/DatabaseConnection.php');
 
 $web_page = file_get_contents('Html/template.html');
 
-/*$chitarra_selezionata=;*/
+$id_selezionato=2;
+$conn=new DatabaseConnection();
+$query='select * from chitarra,prodotto,produttore where chitarra.cod_chitarra=prodotto.codice_prodotto and prodotto.produttore=produttore.ragione_sociale and cod_chitarra='.$id_selezionato.';';
+echo $query;
+$chitarra_selezionata= mysqli_fetch_all($conn->get_result_query($query), MYSQLI_ASSOC);
+
 
 $web_page = str_replace('<title_page/>', "Specifiche prodotto", $web_page);
 
@@ -16,9 +21,12 @@ $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
 $web_page = str_replace('<breadcrumbs_to_insert/>', "Prodotti/Specifiche prodotto", $web_page);
 
+foreach($chitarra_selezionata as $c)
+{
 $filtri=file_get_contents('Html/Filtri.html');
-$contenuto=$filtri.'<div id="specifiche_prodotto"><h1>Specifiche prodotto</h1><p>gregrgegeg</p></br></br></br></br></br></br></div>';
+$contenuto=$filtri.'<div id="specifiche_prodotto"><h1>'.$c['produttore'].' '.$c['modello'].'</h1></div>';
 
+}
 $web_page = str_replace('<contenuto_to_insert/>', $contenuto, $web_page);
 
 echo $web_page;
