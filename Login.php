908e209e336e540dd784f39900f2a1ad0993e4f1
matteobudai <tisco99@hotmail.it>
2020-12-29 11:59:37 +0100

    Sistemato form accedi e creato form registrati

---
+++ Login.php
@@ -0,0 +1,19 @@
+<?php
+
+$web_page = file_get_contents('Html/Template.html');
+
+$web_page = str_replace('<title_page/>', "Accedi", $web_page);
+
+$nav_bar = '       <li class="link" role="none" xml:lang="en"><a href="Home.php" role="menuitem">Home</a></li> 
+<li class="link" role="none"><a href="Prodotti.php?categoria=chitarre" role="menuitem">Prodotti</a></li>
+<li class="link" role="none"><a href="Servizi.php" role="menuitem">Servizi</a></li>
+<li class="link" role="none"><a href="Chisiamo.php" role="menuitem">Chi siamo</a></li>';
+$web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
+
+$web_page = str_replace('<breadcrumbs_to_insert/>','Accedi', $web_page);
+
+$web_page = str_replace('<contenuto_to_insert/>', file_get_contents('Html/Login.html'), $web_page);
+
+echo $web_page;
+
+?>
