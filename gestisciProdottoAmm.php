74d10a979115d6b4065c4bacf0b4b7d98f9c55f2
matteobudai <tisco99@hotmail.it>
2021-01-21 18:59:23 +0100

    Fine modifica

--- gestisciProdottoAmm.php
+++ gestisciProdottoAmm.php
@@ -33,6 +33,7 @@ $web_page = str_replace('<gestioneAccesso/>', '<form  action="Logout.php" method
 
 
 /*------------------QUERY------------------------------*/
+
 $id_prodotto = $_GET['prodotto'];
 
 $manage_prodotto=new ManageProdotti();
@@ -49,38 +50,63 @@ else
 	$prodotto_selezionato= $manage_prodotto->get_specifiche_accessori($id_prodotto);	
 }
 
+if(isset($_REQUEST['SalvaMod'])){
 
-
-
-
-     if(isset($_REQUEST['SalvaMod'])){
-
+  if($categoria == "accessori"){
+      $produttore = $_POST['produttoreAmmModA'];
+      $tipo = $_POST['tipologiaAmmModA'];
+      }else{
+          $produttore = $_POST['produttoreAmmModC'];
+          $tipo = $_POST['tipologiaAmmModC'];
+          $legno_manico = $_POST['legnoManicoMod'];
+          $legno_corpo = $_POST['legnoCorpoMod'];
+      }
+      $modello = $_POST['modelloMod'];
+      $descrizione = $_POST['descrizioneMod'];
+      #inserire immagine
+          $long_desc = $_POST['long_descMod'];
+          $short_desc = $_POST['short_descMod'];
+          $prezzo_vendita = $_POST['prezzoMod'];
+  
+      $modificaP = new ManageProdotti();
+      $modificaP->modifica_prodP($modello, $produttore, $descrizione, $prezzo_vendita, $id_prodotto);
+      if($categoria == "chitarre"){
+      $modificaC = new ManageProdotti();
+      $modificaC->modifica_prodC($tipo, $legno_manico, $legno_corpo, $id_prodotto);
+      }
       if($categoria == "accessori"){
-          $produttore = $_POST['produttoreAmmModA'];
-          $tipo = $_POST['tipologiaAmmModA'];
-          }else{
-              $produttore = $_POST['produttoreAmmModC'];
-              $tipo = $_POST['tipologiaAmmModC'];
-              $legno_manico = $_POST['legnoManicoMod'];
-              $legno_corpo = $_POST['legnoCorpoMod'];
-          }
-          $modello = $_POST['modelloMod'];
-          $descrizione = $_POST['descrizioneMod'];
-          #inserire immagine
-              $long_desc = $_POST['long_descMod'];
-              $short_desc = $_POST['short_descMod'];
-              $prezzo_vendita = $_POST['prezzoMod'];
-      
-          $modificaP = new ManageProdotti();
-          $modificaP->modifica_prodP($modello, $produttore, $descrizione, $prezzo_vendita);
-          $modificaC = new ManageProdotti();
-          $modificaC->modifica_prodC($tipo, $legno_manico, $legno_corpo);
-          $modificaI = new ManageProdotti();
-          $modificaI->modifica_prodI($immagine, $short_desc, $long_desc);
-           
-           }
+      $modificaA = new ManageProdotti();
+      $modificaA->modifica_prodA($tipo, $id_prodotto);
+      }
+      if(isset($_FILES['image'])){
+        $errors= array();
+        $file_name = $_FILES['image']['name'];
+        $file_size =$_FILES['image']['size'];
+        $file_tmp =$_FILES['image']['tmp_name'];
+        $file_type=$_FILES['image']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+        
+        $extensions= array("jpeg","jpg","png");
+        
+        if(in_array($file_ext,$extensions)=== false){
+           $errors[]="extension not allowed, please choose a JPEG or PNG file.";
         }
-
+        
+        if($file_size > 2097152){
+           $errors[]='File size must be excately 2 MB';
+        }
+        
+        if(empty($errors)==true){
+           move_uploaded_file($file_tmp,"Images/".$file_name);
+           echo "Success";
+        }else{
+           print_r($errors);
+        }
+      $modificaI = new ManageProdotti();
+      $modificaI->modifica_prodI("Images/".$file_name, $short_desc, $long_desc, $id_prodotto);
+      }
+      header('Location: Visualizza_prodotto.php?prodotto='.$id_prodotto.'&tipo='.$categoria);
+       }
 
 $dataMod=file_get_contents('Html/gestisciProdotto.html');
   if($categoria == "accessori"){
@@ -134,8 +160,12 @@ $web_page = str_replace('<prezzoV/>',$prodotto_selezionato['prezzo'], $web_page)
 $web_page = str_replace('<descrCV/>',$prodotto_selezionato['short_desc'], $web_page);
 $web_page = str_replace('<descrLV/>',$prodotto_selezionato['long_desc'], $web_page);
 $web_page = str_replace('<descrProdV/>',$prodotto_selezionato['descrizione'], $web_page);
+$web_page = str_replace('<modelloV/>',$prodotto_selezionato['modello'], $web_page);
+$web_page = str_replace('<catModF/>',$categoria, $web_page);
+$web_page = str_replace('<proModF/>',$id_prodotto, $web_page);
 
 echo $web_page;
+
 }
 
 ?>
