2c95a7f30ce4218b80aa955aee5bdafdb96cacf3
matteobudai <tisco99@hotmail.it>
2021-01-23 00:38:55 +0100

    errori registrazione

--- Registrati.php
+++ Registrati.php
@@ -13,7 +13,52 @@ $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
 $web_page = str_replace('<breadcrumbs_to_insert/>','Registrati', $web_page);
 
-$web_page = str_replace('<contenuto_to_insert/>', file_get_contents('Html/Registrati.html'), $web_page);
+$data=file_get_contents('Html/Registrati.html');
+
+
+
+if (isset($_POST['Registrati'])) {
+    $email=$_POST['email'];
+    $username=$_POST['username'];
+    $password=$_POST['password']; 
+    $ridpassword=$_POST['ridigitaPassword']; 
+
+    $database = new DatabaseConnection();
+    $username = stripslashes($username);
+    $username = $database->escape_string($username);
+               
+    $utente = mysqli_fetch_assoc($database->get_result_query("select username, password,email, permessi from user where username='$username' "));
+   
+if($utente!=NULL){
+    $data = str_replace('<errorePassERidPassReg/>', 'Username già in uso', $data);
+    $data = str_replace('valueUserReg', 'value="'.$username.'"', $data);
+        $data = str_replace('valueEmailReg', 'value="'.$email.'"', $data);
+}else{
+
+    if (strlen($password) <3 || strlen($username) <3 || strlen($email) ==0 || ($password)!=($ridpassword)){
+        if(strlen($username) <3){
+            $data = str_replace('<erroreUserReg/>', 'Inserisci almeno 3 caratteri', $data);
+        }
+        if(strlen($password) <3){
+            $data = str_replace('<errorePassReg/>', 'Inserisci almeno 3 caratteri', $data);
+        }
+        if(($password)!=($ridpassword)){
+            $data = str_replace('<errorePassERidPassReg/>', 'Password e Ridigita password devono essere uguali', $data);
+        }
+        if(strlen($email) ==0){
+            $data = str_replace('<erroreEmailReg/>', 'Inserisci una mail corretta', $data);
+        }
+        $data = str_replace('valueUserReg', 'value="'.$username.'"', $data);
+        $data = str_replace('valueEmailReg', 'value="'.$email.'"', $data);
+    }
+    else{
+        $reg = new ManageRegistrati();
+       $reg->registrati($email, $username, $password);
+       header('Location: Login.php ');
+        }
+}
+}
+$web_page = str_replace('<contenuto_to_insert/>',$data , $web_page);
 
 echo $web_page;
 
