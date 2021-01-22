cfa6da832a987f0d60be3b579b8dd2378635c034
matteobudai <tisco99@hotmail.it>
2021-01-22 21:45:58 +0100

    inserisci commento sistemazione

--- Inserisci_commento.php
+++ Inserisci_commento.php
@@ -10,9 +10,9 @@ $web_page = file_get_contents('Html/Template.html');
 
 $web_page = str_replace('<title_page/>', "Inserisci commento", $web_page);
 
-$nav_bar = '       <li class="link" role="none" xml:lang="en"><a href="Home.php" role="menuitem">Home</a></li> 
+$nav_bar = '       <li  class="link" role="none" xml:lang="en"><a class="a_header" href="Home.php" role="menuitem">Home</a></li> 
 <li class="link" role="none"><a class="a_header" href="Prodotti.php?categoria=chitarre" role="menuitem">Prodotti</a></li>
-<li class="link" role="none"><a class="a_header" href="Servizi.php" role="menuitem">Servizi</a></li>
+<li  id="linkCorrente" class="link" role="none">Servizi</li>
 <li class="link" role="none"><a class="a_header" href="Chisiamo.php" role="menuitem">Chi siamo</a></li>';
 $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
