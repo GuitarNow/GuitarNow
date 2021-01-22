81a3829153c58c6d14b35d1525d20cdd5b6f1ff3
matteobudai <tisco99@hotmail.it>
2021-01-22 23:51:29 +0100

    messaggi errore login

--- Login.php
+++ Login.php
@@ -1,7 +1,7 @@
 <?php
+require_once('PHP/back/DatabaseConnection.php');
 
-require_once('PHP/back/ManageLogin.php'); // includo ManageLogin php
-
+session_start();
 if(isset($_SESSION['login_user'])){
     header("location: home.php");
     $_SESSION['cnt'] = 0;
@@ -21,7 +21,7 @@ $web_page = str_replace('<menu_to_insert/>', $nav_bar, $web_page);
 
 $web_page = str_replace('<breadcrumbs_to_insert/>','Accedi', $web_page);
 
-$web_page = str_replace('<contenuto_to_insert/>', file_get_contents('Html/Login.html'), $web_page);
+
 if(isset($_GET['operazione']) && $_GET['operazione']==1)
 {
     $web_page=str_replace('<messaggio/>', '<p>Registrazione eseguita correttamenete.</p>', $web_page);
@@ -30,6 +30,57 @@ else
 {
     $web_page=str_replace('<messaggio/>', '', $web_page);
 }
+
+$data=file_get_contents('Html/Login.html');
+
+if (isset($_POST['submit'])) {
+    $username=$_POST['username'];
+    $password=$_POST['password'];  
+    if (strlen($password) == 0 && strlen($username) ==0 ) {
+        $data = str_replace('<errorePassLogin/>', 'Inserisci un valore', $data);
+        $data = str_replace('<erroreUserLogin/>', 'Inserisci un valore', $data);
+        } 
+if (strlen($username) ==0) {
+$data = str_replace('<erroreUserLogin/>', 'Inserisci un valore', $data);
+}
+else
+    if (strlen($password) == 0) {
+        $data = str_replace('<errorePassLogin/>', 'Inserisci un valore', $data);
+        $data = str_replace('valueUsername', 'value="'.$username.'"', $data);
+        }
+        else
+            if (strlen($password) < 3) {
+                $data = str_replace('<errorePassLogin/>', 'Password di almeno 3 caratteri', $data);
+                $data = str_replace('valueUsername', 'value="'.$username.'"', $data);
+            }else{
+
+
+                $database = new DatabaseConnection();
+                // proteggere MySQL injection 
+                $username = stripslashes($username);
+                $password = stripslashes($password);
+                $username = $database->escape_string($username);
+                $password = $database->escape_string($password);
+$utente = mysqli_fetch_assoc($database->get_result_query("select username, password, permessi from user where (password='$password' AND username='$username') "));	
+
+if ($utente==NULL) {
+    $data = str_replace('<erroreLogin/>', 'Password o Username non corretti', $data);
+    $data = str_replace('valueUsername', 'value="'.$username.'"', $data);
+
+} else {
+    $_SESSION['login_user']=$username; 
+$_SESSION['psw']=$password;
+$_SESSION['permessi']=$utente['permessi'];
+header("location: Home.php"); // indirizzamento
+}
+
+}
+}
+
+
+
+$web_page = str_replace('<contenuto_to_insert/>', $data, $web_page);
+
 echo $web_page;
 
 
