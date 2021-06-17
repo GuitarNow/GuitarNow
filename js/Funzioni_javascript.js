8a862aef38a8dac401d993c9f02ab119508935c0
matteobudai <tisco99@hotmail.it>
2021-06-17 19:21:19 +0200

    Sistemato bug chi siamo e sistemazione menu

--- js/Funzioni_javascript.js
+++ js/Funzioni_javascript.js
@@ -90,7 +90,7 @@ function menuOn(){
         menu.classList.remove('menu');
         menu.classList.add('menu_on');
 
-        for(let i=0;i<=3;i++){
+        for(let i=0;i<4;i++){
             document.getElementsByClassName('a_header')[i].classList.add('a_on');
         }
         link_c.classList.add('link_c');
@@ -104,7 +104,7 @@ function menuOn(){
         close.classList.remove('dU');
         
     
-        for(let i=0;i<3;i++){
+        for(let i=0;i<4;i++){
             document.getElementsByClassName('a_header')[i].classList.remove('a_on');
         }
         link_c.classList.remove('link_c');
